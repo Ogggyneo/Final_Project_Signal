ccde393ea8fe69ce3fa65a4708a8f242f35d475e
leoleo <qp713223@gmail.com>
2026-05-20 16:14:04 +0700

    add git build board

--- Final_Project_ENG/src/main.cpp
+++ Final_Project_ENG/src/main.cpp
@@ -1,9 +1,22 @@
+// #include <Arduino.h>
+// #include "D:\SPRING(1)\ENG\Final_Project_Signal\Final_Project_ENG\src\components\bmi.h"
+// void loop(){
+//   //BMI
+//     int16_t ax, ay, az;
+//     readBMI160(ax, ay, az);
+//     float acc = calculateAccelerationMagnitude(ax, ay, az);
+//     detectStep(acc);
+//     Serial.print("Step Count: ");
+// }
 #include <Arduino.h>
-#include "D:\SPRING(1)\ENG\Final_Project_Signal\Final_Project_ENG\src\components\bmi.h"
-void loop(){
-  //BMI
-    int16_t ax, ay, az;
-    readBMI160(ax, ay, az);
-    float acc = calculateAccelerationMagnitude(ax, ay, az);
-    detectStep(acc);
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    Serial.println("YOLO UNO WORKING");
+    delay(1000);
 }
